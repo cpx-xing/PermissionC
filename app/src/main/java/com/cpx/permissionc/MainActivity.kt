324b5ea8b317b84c408749a78c8ac87b33795c8e
xing_mac <2625185550@qq.com>
2022-09-21 23:41:07 +0800

    权限

--- app/src/main/java/com/cpx/permissionc/MainActivity.kt
+++ app/src/main/java/com/cpx/permissionc/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.cpx.permissionc
 
+
+import android.Manifest.permission.CALL_PHONE
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val testBtn:Button = findViewById(R.id.test)
+
+        testBtn.setOnClickListener{
+            PermissionC.request(this,android.Manifest.permission.CALL_PHONE){
+                allGranted,deniedList ->
+                if (allGranted){
+                    call()
+                }else{
+                    Toast.makeText(this,"Yout denied $deniedList",Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    fun call(){
+        println("call")
     }
 }
---
+++ permissionc/src/main/java/com/cpx/permissionc/PermissionC.kt
@@ -0,0 +1,21 @@
+package com.cpx.permissionc
+
+import android.app.Activity
+import androidx.fragment.app.FragmentActivity
+
+object PermissionC {
+    private const val TAG = "InvisibleFragment"
+
+    fun request(activity: FragmentActivity,vararg permissions:String,callback: PermissionCallback){
+        val fragmentManager = activity.supportFragmentManager
+        val existedFragment = fragmentManager.findFragmentByTag(TAG)
+        val fragment = if (existedFragment != null){
+            existedFragment as InvisibleFragment
+        }else{
+            val invisibleFragment = InvisibleFragment()
+            fragmentManager.beginTransaction().add(invisibleFragment, TAG).commitNow()
+            invisibleFragment
+        }
+        fragment.requestNow(callback,*permissions)
+    }
+}
